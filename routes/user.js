35606de09392b08cd05cf19b2857681972188bcb
shahrukhkh <khanshahrukh7910@gmail.com>
2023-04-11 11:45:53 +0530

    fixed redirect

--- routes/user.js
+++ routes/user.js
@@ -16,9 +16,8 @@ router.get("/login",
     passport.authenticate("google",{
         successRedirect:process.env.FRONTEND_URL,
     }),
-(req,res,next)=>{
-    res.send("Logged In");
-});
+
+);
 router.get("/me",isAuthenticated,myProfile);
 
 router.get("/logout",logout);
